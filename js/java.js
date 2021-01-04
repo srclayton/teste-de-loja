a619de278234bc79ba6a4269a461b7cac97dfb7f
Clayton <56970620+srclayton@users.noreply.github.com>
2021-01-04 11:19:35 -0300

    atualizando o conteudo do carrinho

--- js/java.js
+++ js/java.js
@@ -3,14 +3,23 @@ var itens = 0
     function addItem(valor){
         total = total + valor
         att_qnt_itens()
-        att_total_carrinho(total)
+        att_total_carrinho(total, itens)
     }
 
     function att_qnt_itens(){
         itens = itens + 1
         document.getElementById("total-itens").innerHTML="<img class='img-carrinho' src='img/sacolas-de-compras(1).png'> " + itens + " itens";
     }
-    function att_total_carrinho(valor){
+    function att_total_carrinho(valor,nItens){
         valor = valor.toFixed(2);
+        document.getElementById("test").innerHTML="";
         document.getElementById("total-carrinho").innerHTML="Total R$ " + valor;
+        document.getElementById("conteudo-carrinho").innerHTML+="<div class='row shadow'>"+
+                                                                    "<div class='col-3'>"+
+                                                                        "<img src='img/images.jpg' style='width:100%'></img>"+
+                                                                    "</div>"+
+                                                                    "<div class='col-9'>"+
+                                                                        "cucucucucucucuucu"+
+                                                                    "</div>"+
+                                                                "</div><hr>";                                                                 
     }
