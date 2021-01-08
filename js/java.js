102d4100addba6fe6f933d2f8508543982a35d96
Clayton <56970620+srclayton@users.noreply.github.com>
2021-01-07 22:47:21 -0300

    Passando o nome da roupa por parametro para o carrinho
    ainda não feito com exito.

--- js/java.js
+++ js/java.js
@@ -1,25 +1,27 @@
 var total = 0
 var itens = 0
-    function addItem(valor){
+    function addItem(valor,nButton){
         total = total + valor
         att_qnt_itens()
-        att_total_carrinho(total, itens)
+        att_total_carrinho(valor, total, itens)
     }
 
     function att_qnt_itens(){
         itens = itens + 1
         document.getElementById("total-itens").innerHTML="<img class='img-carrinho' src='img/sacolas-de-compras(1).png'> " + itens + " itens";
     }
-    function att_total_carrinho(valor,nItens){
+    function att_total_carrinho(valor, total, itens){
         valor = valor.toFixed(2);
         document.getElementById("test").innerHTML="";
-        document.getElementById("total-carrinho").innerHTML="Total R$ " + valor;
+        document.getElementById("total-carrinho").innerHTML="Total R$ " + total + " " + itens + " itens";
         document.getElementById("conteudo-carrinho").innerHTML+="<div class='row shadow'>"+
                                                                     "<div class='col-3'>"+
                                                                         "<img src='img/images.jpg' style='width:100%'></img>"+
                                                                     "</div>"+
                                                                     "<div class='col-9'>"+
-                                                                        "cucucucucucucuucu"+
+                                                                    "<strong id='titulo-da-roupa-carrinho'> </strong>" + valor +
                                                                     "</div>"+
-                                                                "</div><hr>";                                                                 
+                                                                "</div><hr>";
+        nome= document.getElementById("titulo-da-roupa").textContent;
+        document.getElementById("titulo-da-roupa-carrinho").innerHTML= nome;                                                                 
     }
