50c8091be910f5c45d27283df4f1cde8853616c1
Clayton <56970620+srclayton@users.noreply.github.com>
2021-01-29 22:05:56 -0300

    updt

--- js/java.js
+++ js/java.js
@@ -1,16 +1,17 @@
 var total = 0
 var itens = 0
-    function addItem(valor,nButton){
+const nameItens = {1:'Conjunto de roupas 1', 2:'Conjunto de roupas 2', 3:'Conjunto de roupas 3', 4:'Conjunto de roupas 4'}
+    function addItem(valor,nomeItem){
         total = total + valor
         att_qnt_itens()
-        att_total_carrinho(valor, total, itens)
+        att_total_carrinho(valor, total, itens, nomeItem)
     }
 
     function att_qnt_itens(){
         itens = itens + 1
         document.getElementById("total-itens").innerHTML="<img class='img-carrinho' src='img/sacolas-de-compras(1).png'> " + itens + " itens";
     }
-    function att_total_carrinho(valor, total, itens){
+    function att_total_carrinho(valor, total, itens, nomeItem){
         valor = valor.toFixed(2);
         document.getElementById("test").innerHTML="";
         document.getElementById("total-carrinho").innerHTML="Total R$ " + total + " " + itens + " itens";
@@ -19,9 +20,7 @@ var itens = 0
                                                                         "<img src='img/images.jpg' style='width:100%'></img>"+
                                                                     "</div>"+
                                                                     "<div class='col-9'>"+
-                                                                    "<strong id='titulo-da-roupa-carrinho'> </strong>" + valor +
+                                                                    "<strong id='titulo-da-roupa-carrinho'> </strong>" + valor + nameItens[nomeItem] +
                                                                     "</div>"+
-                                                                "</div><hr>";
-        nome= document.getElementById("titulo-da-roupa").textContent;
-        document.getElementById("titulo-da-roupa-carrinho").innerHTML= nome;                                                                 
+                                                                "</div><hr>";                                                                
     }
